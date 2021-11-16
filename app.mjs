f1d1f6ab6f64ec428cbb9279dd59c64983bb7c64
Ahmed <ahmed.jamil7410@gmail.com>
2021-11-16 06:18:34 +0500

    Added soem features

--- app.mjs
+++ app.mjs
@@ -2,12 +2,70 @@ import express from "express";
 import { Server } from "socket.io";
 import http from "http";
 import path from "path";
+import Mongoose from "mongoose";
+import { StringDecoder } from "string_decoder";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
+const uri = `mongodb+srv://admin:admin@cluster0.uj1ij.mongodb.net/scorecard`;
 const __dirname = path.resolve();
+const scoreSchema = new Mongoose.Schema({
+  side1: {
+    name: String,
+    batting: Boolean,
+    bowling: Boolean,
+  },
+  side2: {
+    name: String,
+    batting: Boolean,
+    bowling: Boolean,
+  },
+  wickets: {
+    type: Number,
+  },
+  runs: {
+    type: Number,
+  },
+  batting: {
+    bat1: {
+      name: String,
+      score: Number,
+    },
+    bat2: {
+      name: String,
+      score: Number,
+    },
+    onStrike: Number,
+  },
+  overs: {
+    type: Number,
+  },
+  bowling: {
+    currBowler: String,
+    oversBowled: Number,
+    wicketTaken: Number,
+  },
+});
+
+const scoreCardModel = Mongoose.model("Scorecard", scoreSchema);
+const scores = new scoreCardModel({
+  side1: "Australia",
+  side2: "Pakistan",
+  wickets: 5,
+  runs: 110,
+  batting: {
+    bat1: { name: "Babar Azam", score: 56 },
+    bat2: { name: "Muhammad Rizwan", score: 95 },
+    onStrike: 1,
+  },
+  overs: 50,
+  bowling: {
+    currBowler: "Imran Tahir",
+    oversBowled: 10,
+    wicketsTaken: 5,
+  },
+});
 
 io.on("connection", (stream) => {
   console.log("Connected");
@@ -15,15 +73,32 @@ io.on("connection", (stream) => {
     console.log(stream.id);
     console.log(msg);
     stream.broadcast.emit("chat_message", msg);
-  })
+  });
   stream.on("disconnect", () => console.log("Diconnected"));
 });
-io
-
-app.get("/", (_, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get("/score", (req, res, next) => {
+  res.status(200).json({
+    side1: user.side1,
+    side2: user.side2,
+    wickets: user.wickets,
+    runs: user.runs,
+    batting: user.batting,
+    overs: 50,
+    bowling: user.bowling,
+  });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
-});
+Mongoose.connect(uri, { useUnifiedTopology: true })
+  .then(() => {
+    server.listen(5000, () => {
+      console.log("listening on *:5000");
+    });
+    console.log("Database connected");
+    scores.save((err) => {
+      if (err) console.error(err);
+    });
+  })
+  .catch((err) => {
+    console.error("Could not connect to database");
+    console.error(err);
+  });
--- web/src/component/Card.jsx
+++ web/src/component/Card.jsx
@@ -1,18 +1,31 @@
-import React, { useEffect } from 'react';
-import styles from './Card.module.css';
+import React, { useEffect } from "react";
+import styles from "./Card.module.css";
 import { io } from "socket.io-client";
-
 const Card = (props) => {
-    useEffect(()=> {
+  useEffect(() => {
+  }, []);
 
-        return () => io.close();
-    }, [])
-
-
-
-
-    return (<div className={styles.container}>
-        <h1>Aus vs Pakistan</h1>
-    </div>);
-}
+  return (
+    <div className={styles.container}>
+      <h1>Aus vs Pakistan</h1>
+      <div className={styles.batting}>
+        <h2>Batting Side</h2>
+        <p>185/1</p>
+        <p>15.5</p>
+        <div className ={styles.batters}>
+          <p>
+            Andrew Fletcher <span></span>
+          </p>
+          <p>
+            Light yagami <span></span>
+          </p>
+        </div>
+      </div>
+      <div className={styles.bowling}>
+          <h2>Bowling Side</h2>
+          <p>Imran Tahir <span>5.5 overs</span> <span>2 </span> <span> 23 run</span></p>
+      </div>
+    </div>
+  );
+};
 export default Card;
